4fe9f186ac45f96e4eb2f5e820210d589e34b23c
Jason Zhang <jasonjzhang17@gmail.com>
2022-09-03 09:57:34 -0400

    Create files for eda functions

---
+++ src/geeda/dataframe/check_nan.py
@@ -0,0 +1,5 @@
+
+
+
+def check_nan():
+    pass
--- src/geeda/dataframe/geeda.py
+++ src/geeda/dataframe/geeda.py
@@ -8,11 +8,19 @@ class Geeda:
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
 
-    def apply(self, columns: Union[str, List[str]], column_functions: Union[Callable, List[Callable]], df_functions: Union[Callable, List[Callable]]) -> None:
+    def apply(
+        self,
+        columns: Union[str, List[str]],
+        column_functions: Union[Callable, List[Callable]],
+        df_functions: Union[Callable, List[Callable]],
+    ) -> None:
         validate_columns(df=self.df, columns=columns)
         columns = make_list(columns)
         functions = make_list(functions)
 
         for function in column_functions:
             for column in columns:
-                function(df[column])
+                function(self.df[column])
+
+        for function in df_functions:
+            function(self.df)
