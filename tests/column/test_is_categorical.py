0dae7f9592ab92b882d66d3da66c185b210b61de
Jason Zhang <jasonjzhang17@gmail.com>
2022-09-04 17:41:54 -0400

    Add tests for coverage for is_categorical

--- tests/column/test_is_categorical.py
+++ tests/column/test_is_categorical.py
@@ -5,9 +5,19 @@ from src.geeda.column.is_categorical import is_categorical
 
 
 @pytest.mark.parametrize(
-    argnames=["column", "max_threshold", "expected"],
-    argvalues=[[pd.Series([1, 1, 1, 1, 1, 2, 2, 2, 2, 2]), 0.3, True]],
-    ids=["1"],
+    argnames="column, max_threshold, expected",
+    argvalues=[
+        [pd.Series([1, 1, 1, 1, 1, 2, 2, 2, 2, 2]), 0.3, True],
+        [pd.Series([1, 1, 1, 2, 2, 3, 3, 3, 4, 4]), 0.5, True],
+        [pd.Series([1, 1, 1, 2, 2, 3, 3, 3, 4, 4]), 0.3, False],
+        [pd.Series([1, 2, 3, 4, 5]), 0.3, False],
+    ],
+    ids=[
+        "Categorical with 2 unique values",
+        "Categorical with 4 unique values",
+        "Not categorical with 4 unique values",
+        "Not categorical with all unique values",
+    ],
 )
 def test_is_categorical(column, max_threshold, expected):
     assert is_categorical(column, max_threshold) == expected
---
+++ tests/dataframe/test_geeda.py
@@ -0,0 +1,26 @@
+import pytest
+import pandas as pd
+
+from src.geeda.dataframe.geeda import Geeda
+from src.geeda.column.is_categorical import is_categorical
+
+
+@pytest.mark.parametrize(
+    argnames="df, columns, eda_functions, expected",
+    argvalues=[
+        [pd.DataFrame({"a": range(1, 6), "b": range(6, 11)})],
+        "a",
+        is_categorical,
+        "True",
+    ],
+    ids=["Basic"],
+)
+def test_geeda(df, columns, eda_functions, expected, capsys):
+    # Arrange
+    geeda = Geeda(df)
+
+    # Act
+    geeda.apply(columns, eda_functions)
+
+    # Assert
+    assert expected in capsys.readouterr().out
