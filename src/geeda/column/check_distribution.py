d8c89f7adf7c4e80522566e258941440a64b403c
Jason Zhang <jasonjzhang17@gmail.com>
2022-10-15 10:47:54 -0400

    Implement uniformity test

--- src/geeda/column/check_distribution.py
+++ src/geeda/column/check_distribution.py
@@ -1,11 +1,18 @@
 import pandas as pd
+from scipy import stats
 from typing import Tuple
 
 
-def check_distribution() -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """_summary_
+def check_distribution(column: pd.Series) -> str:
+    # Use stats.kstest() for uniform, shapiro_wilk for normal
+    mean = column.mean()
+    std = column.std()
+    size = column.size
 
-    Returns:
-        Tuple[pd.DataFrame, pd.DataFrame]: _description_
-    """
+    # Normality Test
+
+    # Uniformity Test
+    uniform = stats.uniform.rvs(loc=mean, scale=std, size=size, random_state=None)
+    stats.kstest(rvs=column, cdf=uniform)
+    # stats.kstest(rvs=column, cdf="uniform")
     pass
