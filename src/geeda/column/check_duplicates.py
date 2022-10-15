5d93409ef3e3929897e76b9f588bc409f2f32571
Jason Zhang <jasonjzhang17@gmail.com>
2022-10-15 10:47:54 -0400

    Implement uniformity test

--- src/geeda/column/check_duplicates.py
+++ src/geeda/column/check_duplicates.py
@@ -1,11 +1,9 @@
-from re import I
 import pandas as pd
 
 from geeda.dataframe.geeda import Geeda
 from geeda.column.is_categorical import is_categorical
 from geeda.column.check_inf import check_inf
 from geeda.dataframe.check_duplicates import check_duplicates
-from geeda.utils import print_df
 
 
 df = pd.DataFrame(
